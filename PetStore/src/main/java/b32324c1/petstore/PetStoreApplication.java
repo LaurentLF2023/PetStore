7680f4febba28bb23b6bd5b8c555a764cd39ec63
Laurent Le Foulgoc <laurent.le-foulgoc@epitech.eu>
2024-02-22 15:59:14 +0100

    initial commit

---
+++ PetStore/src/main/java/b32324c1/petstore/PetStoreApplication.java
@@ -0,0 +1,13 @@
+package b32324c1.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+	}
+
+}
---
+++ PetStore/src/test/java/b32324c1/petstore/PetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package b32324c1.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
